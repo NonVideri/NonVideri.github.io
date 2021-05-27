e57e06a662bc16073df1925cf53e7c65b556f6ac
NonVideri <krzysztof.adam.moszynski@gmail.com>
2021-05-27 10:38:01 +0200

    Add ContactManager

---
+++ React/src/ContactManager.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import AddPersonForm from './AddPersonForm';
+import PeopleList from './PeopleList'
+
+export default function ContactManager(props) {
+  const [contacts, setContacts] = useState(props.data);
+
+  function addPerson(name) {
+    setContacts([...contacts, name]);
+  }
+
+  return (
+    <div>
+      <AddPersonForm handleSubmit={addPerson} />
+      <PeopleList data={contacts} />
+    </div>
+  );
+}
