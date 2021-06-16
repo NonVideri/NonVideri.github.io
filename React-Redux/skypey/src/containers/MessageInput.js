a3ce7a4893ab0aae0fa1e2f94ff7a0d4508702dc
NonVideri <krzysztof.adam.moszynski@gmail.com>
2021-06-16 14:57:48 +0200

    Skypey: Add base for message sending functionality

--- React-Redux/skypey/src/containers/MessageInput.js
+++ React-Redux/skypey/src/containers/MessageInput.js
@@ -1,6 +1,6 @@
 import React from "react";
 import store from "../store";
-import { setTypingValue } from "../actions";
+import { setTypingValue, sendMessage } from "../actions";
 import "./MessageInput.css";
 
 const MessageInput = ({ value }) => {
@@ -9,8 +9,16 @@ const MessageInput = ({ value }) => {
     store.dispatch(setTypingValue(e.target.value));
   };
 
+  const state = store.getState();
+
+  const handleSubmit = e => {
+      e.preventDefault();
+      const { typing, activeUserId } = state;
+      store.dispatch(sendMessage(typing, activeUserId));
+    };
+
   return (
-    <form className="Message">
+    <form className="Message" onSubmit={handleSubmit}>
       <input
         className="Message__input"
         onChange={handleChange}
