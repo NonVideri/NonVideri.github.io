b6aa4472527e25ac6370b3d606b8907402cb2510
NonVideri <krzysztof.adam.moszynski@gmail.com>
2021-06-17 20:05:28 +0200

    Recipes: Add favorite recipes list

--- React-Redux/recipes/src/features/favoriteRecipes/FavoriteRecipes.js
+++ React-Redux/recipes/src/features/favoriteRecipes/FavoriteRecipes.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import Recipe from "../../components/Recipe";
 
 export default function FavoriteRecipes(props) {
+
+  const { favoriteRecipes, dispatch } = props;
+
   return (
-    <div className="recipes-container"></div>
+    <div className="recipes-container">
+      {favoriteRecipes.map((recipe) => (
+        <Recipe recipe={recipe} key={recipe.id}></Recipe>
+      ))}
+    </div>
   )
 }
