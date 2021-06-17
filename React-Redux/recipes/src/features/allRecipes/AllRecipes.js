a189094b30214c8763533afce070f66fc5be16c7
NonVideri <krzysztof.adam.moszynski@gmail.com>
2021-06-17 20:23:16 +0200

    Recipes: Add adding to Favorites functionality

--- React-Redux/recipes/src/features/allRecipes/AllRecipes.js
+++ React-Redux/recipes/src/features/allRecipes/AllRecipes.js
@@ -1,7 +1,12 @@
 import React, { useEffect } from 'react';
-import { loadData } from './allRecipesSlice'
 
+import { addRecipe } from '../favoriteRecipes/favoriteRecipesSlice.js';
+import { loadData } from './allRecipesSlice';
+
+import FavoriteButton from '../../components/FavoriteButton';
 import Recipe from '../../components/Recipe';
+
+const favoriteIcon = '../../img/favorite.svg';
 
 export default function AllRecipes(props) {
   const { allRecipes, dispatch } = props;
@@ -11,10 +16,21 @@ export default function AllRecipes(props) {
   }
   useEffect(onFirstRender, [])
 
+  const onAddRecipeHandler = (recipe) => {
+    dispatch(addRecipe(recipe));
+  };
+
   return (
     <div className="recipes-container">
       {allRecipes.map((recipe) => (
-        <Recipe recipe={recipe} key={recipe.id}></Recipe>
+        <Recipe recipe={recipe} key={recipe.id}>
+          <FavoriteButton
+            onClickHandler={() => onAddRecipeHandler(recipe)}
+            icon={favoriteIcon}
+          >
+            Add to Favorites
+          </FavoriteButton>
+        </Recipe>
       ))}
     </div>
   )
