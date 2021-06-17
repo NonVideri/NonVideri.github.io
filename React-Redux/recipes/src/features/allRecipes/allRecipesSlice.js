bd32ba0e2cae72916d5bd1dda63224d5dc3c009d
NonVideri <krzysztof.adam.moszynski@gmail.com>
2021-06-17 19:39:15 +0200

    Recipes: Add allRecipes slice

--- React-Redux/recipes/src/features/allRecipes/allRecipesSlice.js
+++ React-Redux/recipes/src/features/allRecipes/allRecipesSlice.js
@@ -1,5 +1,26 @@
-const initialState = [];
+import allRecipesData from '../../data.js'
+import { LOAD_DATA, ADD_RECIPE, REMOVE_RECIPE } from '../../action-types'
 
+// Action creators
+export const loadData = () => {
+  return {
+    type: LOAD_DATA,
+    payload: allRecipesData
+  }
+}
+
+const initialState = [];
 export default function allRecipesReducer (allRecipes = initialState, action) {
-  return allRecipes;
+  switch (action.type) {
+    // Loads recipes data
+    case LOAD_DATA:
+      return action.payload;
+    // Adding recipe to Favorites removes it from All Recipes and vice versa
+    case ADD_RECIPE:
+      return allRecipes.filter(recipe => recipe.id !== action.payload.id);
+    case REMOVE_RECIPE:
+      return [...allRecipes, action.payload]
+    default:
+      return allRecipes;
+  }
 }
