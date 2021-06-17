20de98658a5e7ad90e2b4cf0dfe0987316e46c2e
NonVideri <krzysztof.adam.moszynski@gmail.com>
2021-06-17 20:09:17 +0200

    Recipes: Add favoriteRecipes slice

--- React-Redux/recipes/src/features/favoriteRecipes/favoriteRecipesSlice.js
+++ React-Redux/recipes/src/features/favoriteRecipes/favoriteRecipesSlice.js
@@ -1,5 +1,29 @@
-const initialState = [];
+import { ADD_RECIPE, REMOVE_RECIPE } from "../../action-types";
 
+// Action creators
+export function addRecipe(recipe) {
+  return {
+    type: ADD_RECIPE,
+    payload: recipe
+  }
+}
+
+export function removeRecipe(recipe) {
+  return {
+    type: REMOVE_RECIPE,
+    payload: recipe
+  }
+}
+
+// Reducer 
+const initialState = [];
 export default function favoriteRecipesReducer (favoriteRecipes = initialState, action) {
-  return favoriteRecipes;
+  switch (action.type) {
+    case ADD_RECIPE:
+      return [...favoriteRecipes, action.payload]
+    case REMOVE_RECIPE:
+      return favoriteRecipes.filter(recipe => recipe.id !== action.payload.id)
+    default:
+      return favoriteRecipes;
+  }
 }
