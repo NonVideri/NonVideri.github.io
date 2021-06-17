f5ab80aedd5b17251433b454e3f7604de70930c5
NonVideri <krzysztof.adam.moszynski@gmail.com>
2021-06-17 20:30:16 +0200

    Recipes: Add searchTerm slice

--- React-Redux/recipes/src/features/searchTerm/searchTermSlice.js
+++ React-Redux/recipes/src/features/searchTerm/searchTermSlice.js
@@ -1,5 +1,28 @@
-const initialState = '';
+import { SET_SEARCH_TERM, CLEAR_SEARCH_TERM } from "../../action-types";
 
+// Action creators
+export function setSearchTerm(term) {
+  return {
+    type: SET_SEARCH_TERM,
+    payload: term
+  }
+}
+
+export function clearSearchTerm() {
+  return {
+    type: CLEAR_SEARCH_TERM
+  }
+}
+
+// Reducer
+const initialState = '';
 export default function searchTermReducer (state = initialState, action) {
-  return state;
+  switch (action.type) {
+    case SET_SEARCH_TERM:
+      return action.payload;
+    case CLEAR_SEARCH_TERM:
+      return '';
+    default:
+      return state;
+  }
 }
