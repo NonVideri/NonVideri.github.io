29e91d7984d833b0e14a65ea5876095015738a98
NonVideri <krzysztof.adam.moszynski@gmail.com>
2021-07-15 16:38:49 +0200

    Webpage: Refactor index.scss, change CSS import to SCSS

--- React-Redux/Webpage/src/index.js
+++ React-Redux/Webpage/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import "./style.css";
+import "./style.scss";
 import reportWebVitals from "./reportWebVitals";
 
 import Navigation from "./components/Navigation";
