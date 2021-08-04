25ff3b49f3787a3aac757a87db7070f7f6d068a6
NonVideri <krzysztof.adam.moszynski@gmail.com>
2021-08-04 15:57:09 +0200

    Express Course Project: Add mock people management

--- Node/Express Course Project/app.js
+++ Node/Express Course Project/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 
 const auth = require('./routes/auth')
+const people = require('./routes/people')
 
 app.use(express.static('./public'))
 // Parse form data from JS
@@ -9,6 +10,8 @@ app.use(express.json())
 
 // Authentication route
 app.use('/login', auth)
+// People management route
+app.use('/api/people', people)
 
 app.listen(5000, () => {
   console.log('Server is listening on port 5000...')
