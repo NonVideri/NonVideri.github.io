e3b121667791c000ca17fa82f6cd1a077f3c9ec1
NonVideri <krzysztof.adam.moszynski@gmail.com>
2021-08-05 09:42:53 +0200

    Task Manager: Create tasks route

--- Node/Task Manager/app.js
+++ Node/Task Manager/app.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
+const tasks = require('./routes/tasks')
+
 const port = 3000;
 
-// routes
+// Middleware
+app.use(express.json())
+
+// Routes
 app.get('/hello', (req, res) => {
   res.send('Task Manager App')
 })
+app.use('/api/v1/tasks', tasks)
 
 app.listen(port, console.log(`The server is listening on port ${port}...`));
