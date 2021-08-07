4461a5e3f5eb7b1caa549e080424c6fa6c598898
NonVideri <krzysztof.adam.moszynski@gmail.com>
2021-08-07 12:59:05 +0200

    Task Manager: Add schema and validation

--- Node/Task Manager/controllers/tasks.js
+++ Node/Task Manager/controllers/tasks.js
@@ -1,8 +1,15 @@
+const Task = require("../models/Task");
+
 const getAllTasks = (req, res) => {
   res.send("All tasks.");
 };
-const createTask = (req, res) => {
-  res.send("Create task.");
+const createTask = async (req, res) => {
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  } catch (err) {
+    res.status(500).json({ msg: err });
+  }
 };
 const getTask = (req, res) => {
   res.send("Get single task.");
