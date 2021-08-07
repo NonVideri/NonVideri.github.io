a8327ad2365cbe5895274dca1eab97296a6ad6b9
NonVideri <krzysztof.adam.moszynski@gmail.com>
2021-08-08 00:29:46 +0200

    Task Manager: Add custom port setting

--- Node/Task Manager/app.js
+++ Node/Task Manager/app.js
@@ -16,7 +16,7 @@ app.use("/api/v1/tasks", tasks);
 app.use(notFound); // Default used if another route is used
 app.use(errorHandler);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
