cb9fc707321e9876c75e84b6a2452274fc763492
NonVideri <krzysztof.adam.moszynski@gmail.com>
2021-08-21 19:03:03 +0200

    A&DS: Add bubble sort

---
+++ Node/Algorithms and Data Structures/bubbleSort.js
@@ -0,0 +1,22 @@
+const bubbleSort = input => {
+  let swapping = true;
+
+  while (swapping) {
+    swapping = false;
+    for (let i = 0; i < input.length - 1; i++) {
+      if (input[i] > input[i + 1]) {
+        swap(input, i, i + 1);
+        swapping = true;
+      }
+    }
+  }
+  return input;
+};
+
+const swap = (arr, indexOne, indexTwo) => {
+  const temp = arr[indexTwo];
+  arr[indexTwo] = arr[indexOne];
+  arr[indexOne] = temp;
+};
+
+module.exports = bubbleSort;
