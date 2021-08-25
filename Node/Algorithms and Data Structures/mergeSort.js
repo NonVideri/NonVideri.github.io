674d01c3c5bf08dc94327748261f7e3edb0f6c01
NonVideri <krzysztof.adam.moszynski@gmail.com>
2021-08-25 22:44:44 +0200

    A&DS: Add merge sort

---
+++ Node/Algorithms and Data Structures/mergeSort.js
@@ -0,0 +1,26 @@
+const mergeSort = startArray => {
+  const length = startArray.length;
+  if (length === 1) {
+    return startArray;
+  }
+
+  const mid = Math.floor(length / 2);
+  const leftArray = startArray.slice(0, mid);
+  const rightArray = startArray.slice(mid, length);
+
+  return merge(mergeSort(leftArray), mergeSort(rightArray));
+};
+
+const merge = (leftArray, rightArray) => {
+  const sortedArray = [];
+  while (leftArray.length && rightArray.length) {
+    if (leftArray[0] < rightArray[0]) {
+      sortedArray.push(leftArray.shift());
+    } else {
+      sortedArray.push(rightArray.shift());
+    }
+  }
+  return sortedArray.concat(leftArray).concat(rightArray);
+};
+
+module.exports = mergeSort;
