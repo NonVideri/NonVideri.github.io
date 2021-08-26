eca55da6bde95d6c3fec2123617c8305d823d3d6
NonVideri <krzysztof.adam.moszynski@gmail.com>
2021-08-26 22:43:07 +0200

    RPS: Add move type and value variables

--- Node & JS/Rock Paper Scissors/js/game-logic.js
+++ Node & JS/Rock Paper Scissors/js/game-logic.js
@@ -1,4 +1,17 @@
 // All code should be written in this file.
+let playerOneMoveOneType = undefined;
+let playerOneMoveTwoType = undefined;
+let playerOneMoveThreeType = undefined;
+let playerTwoMoveOneType = undefined;
+let playerTwoMoveTwoType = undefined;
+let playerTwoMoveThreeType = undefined;
+
+let playerOneMoveOneValue = undefined;
+let playerOneMoveTwoValue = undefined;
+let playerOneMoveThreeValue = undefined;
+let playerTwoMoveOneValue = undefined;
+let playerTwoMoveTwoValue = undefined;
+let playerTwoMoveThreeValue = undefined;
 
 const setPlayerMoves = (
   player,
