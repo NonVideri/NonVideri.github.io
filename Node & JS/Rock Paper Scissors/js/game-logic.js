69c9b9e8f819edd7b00c7b4b44ba7a597af4578d
NonVideri <krzysztof.adam.moszynski@gmail.com>
2021-08-26 22:46:16 +0200

    RPS: Add input validation and move setting

--- Node & JS/Rock Paper Scissors/js/game-logic.js
+++ Node & JS/Rock Paper Scissors/js/game-logic.js
@@ -13,6 +13,16 @@ let playerTwoMoveOneValue = undefined;
 let playerTwoMoveTwoValue = undefined;
 let playerTwoMoveThreeValue = undefined;
 
+const moveTypes = ["Rock", "Paper", "Scissors"];
+const playerTypes = ["Player One", "Player Two"];
+const checkNumber = num => {
+  if (num >= 1 && num <= 99) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
 const setPlayerMoves = (
   player,
   moveOneType,
@@ -21,7 +31,40 @@ const setPlayerMoves = (
   moveTwoValue,
   moveThreeType,
   moveThreeValue
-) => {};
+) => {
+  // Validate input
+  if (
+    !(
+      playerTypes.includes(player) &&
+      moveTypes.includes(moveOneType) &&
+      checkNumber(moveOneValue) &&
+      moveTypes.includes(moveTwoType) &&
+      checkNumber(moveTwoValue) &&
+      moveTypes.includes(moveThreeType) &&
+      checkNumber(moveThreeValue) &&
+      checkNumber(moveOneValue + moveTwoValue + moveThreeValue)
+    )
+  ) {
+    return;
+  }
+  // Set moves and values classic style
+  if (player === "Player One") {
+    playerOneMoveOneType = moveOneType;
+    playerOneMoveTwoType = moveTwoType;
+    playerOneMoveThreeType = moveThreeType;
+    playerOneMoveOneValue = moveOneValue;
+    playerOneMoveTwoValue = moveTwoValue;
+    playerOneMoveThreeValue = moveThreeValue;
+  }
+  if (player === "Player Two") {
+    playerTwoMoveOneType = moveOneType;
+    playerTwoMoveTwoType = moveTwoType;
+    playerTwoMoveThreeType = moveThreeType;
+    playerTwoMoveOneValue = moveOneValue;
+    playerTwoMoveTwoValue = moveTwoValue;
+    playerTwoMoveThreeValue = moveThreeValue;
+  }
+};
 
 const getRoundWinner = roundNumber => {};
 
