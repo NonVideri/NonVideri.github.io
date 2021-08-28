75644790136e3beafe3576adac8cfad5b2a0429b
NonVideri <krzysztof.adam.moszynski@gmail.com>
2021-08-28 13:48:01 +0200

    Beat Mix: Add preset handler

--- Node-JS/Beat-Mix/presetHandler.js
+++ Node-JS/Beat-Mix/presetHandler.js
@@ -1,8 +1,18 @@
 // Use this presets array inside your presetHandler
-const presets = require('./presets');
+const presets = require("./presets");
 
 // Complete this function:
-const presetHandler = () => {};
+const presetHandler = (requestType, index, newPresetArray) => {
+  let returnArray = [200];
+  if (!(index >= 0 && index < presets.length)) returnArray[0] = 404;
+  if (!(requestType === "GET" || requestType === "PUT")) returnArray[0] = 400;
+  if (requestType === "GET") returnArray[1] = presets[index];
+  if (requestType === "PUT") {
+    presets[index] = newPresetArray;
+    returnArray[1] = newPresetArray;
+  }
+  return returnArray;
+};
 
 // Leave this line so that your presetHandler function can be used elsewhere:
 module.exports = presetHandler;
