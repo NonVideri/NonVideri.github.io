c41988b4630aeb80305e0e76990c86eb453fbbc0
NonVideri <krzysztof.adam.moszynski@gmail.com>
2021-08-28 12:07:04 +0200

    Beat Mix: Add drum toggling

--- Node-JS/Beat-Mix/public/js/script.js
+++ Node-JS/Beat-Mix/public/js/script.js
@@ -3,3 +3,24 @@ let kicks = Array.from({ length: 16 }, i => false);
 let snares = Array.from({ length: 16 }, i => false);
 let hiHats = Array.from({ length: 16 }, i => false);
 let rideCymbals = Array.from({ length: 16 }, i => false);
+
+const selectDrum = drumName => {
+  switch (drumName) {
+    case "kicks":
+      return kicks;
+    case "snares":
+      return snares;
+    case "hiHats":
+      return hiHats;
+    case "rideCymbals":
+      return rideCymbals;
+    default:
+      return;
+  }
+};
+
+const toggleDrum = (drumName, index) => {
+  if (!(drumName && index >= 0 && index <= 16)) return;
+  const drum = selectDrum(drumName);
+  drum[index] = !drum[index];
+};
