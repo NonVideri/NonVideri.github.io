e8a241dacddb931c9e157ef37170ab694d5d1367
NonVideri <krzysztof.adam.moszynski@gmail.com>
2021-08-31 14:33:03 +0200

    Boss Machine: Add new minion creation

--- Node-JS/Boss-Machine/server/controllers/minionsController.js
+++ Node-JS/Boss-Machine/server/controllers/minionsController.js
@@ -10,12 +10,24 @@ const {
 
 const getMinions = (req, res) => {
   const minions = getAllFromDatabase("minions");
-  if (minions) {
-    return res.status(200).send(minions);
+  if (minions) return res.status(200).send(minions);
+  else res.sendStatus(404);
+};
+
+const createMinion = (req, res) => {
+  const minion = req.body;
+  if (
+    minion &&
+    typeof minion.name === "string" &&
+    typeof minion.title === "string" &&
+    typeof minion.salary === "number"
+  ) {
+    const addedMinion = addToDatabase("minions", minion);
+    res.status(201).send(addedMinion);
   }
-  res.sendStatus(404);
 };
 
 module.exports = {
-  getMinions
+  getMinions,
+  createMinion
 };
