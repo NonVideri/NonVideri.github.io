e7ad94ce79cd76bca1c802b5431c10818e8ceec4
NonVideri <krzysztof.adam.moszynski@gmail.com>
2021-09-05 07:41:47 +0200

    Codestrips: Add testing

--- Node-JS/Codestrips/app.js
+++ Node-JS/Codestrips/app.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
-const sqlite3 = require("sqlite3");
-const db = new sqlite3.Database("./db.sqlite");
 
 app.use(morgan("dev"));
 app.use(express.json());
