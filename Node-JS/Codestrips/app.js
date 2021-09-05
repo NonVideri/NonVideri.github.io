cca47221c0659146dd4c4117e99759d7dbfc5f08
NonVideri <krzysztof.adam.moszynski@gmail.com>
2021-09-05 07:57:11 +0200

    Codestrips: Create database

--- Node-JS/Codestrips/app.js
+++ Node-JS/Codestrips/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const sqlite3 = require("sqlite3");
+const db = new sqlite3.Database(process.env.TEST_DATABASE || "./db.sqlite");
 
 app.use(morgan("dev"));
 app.use(express.json());
--- Node-JS/Codestrips/sql.js
+++ Node-JS/Codestrips/sql.js
@@ -1,2 +1,12 @@
 const sqlite3 = require("sqlite3");
 const db = new sqlite3.Database("./db.sqlite");
+
+db.run(`CREATE TABLE Strip (
+  id integer PRIMARY KEY,
+  head text NOT NULL,
+  body text NOT NULL,
+  background text NOT NULL,
+  bubble_type text NOT NULL,
+  bubble_text text NOT NULL DEFAULT "",
+  caption text NOT NULL DEFAULT ""
+)`);
