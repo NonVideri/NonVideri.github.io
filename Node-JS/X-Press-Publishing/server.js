c7fc1d80911cbcf7fabcb462ef826ab000623b74
NonVideri <krzysztof.adam.moszynski@gmail.com>
2021-09-06 17:05:02 +0200

    XPP: Create basic app structure

---
+++ Node-JS/X-Press-Publishing/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const errorHandler = require("errorhandler");
+const app = express();
+const PORT = process.env.PORT || 4001;
+
+app.use(express.json());
+app.use(errorHandler());
+app.use(cors());
+app.use(morgan("dev"));
+
+app.listen(PORT, () => {
+  console.log(`Server running at port ${PORT}...`);
+});
+
+module.exports = app;
