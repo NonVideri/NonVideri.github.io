9e69590b3fe7cbe4614762a359b4a35ffac5dc2b
NonVideri <krzysztof.adam.moszynski@gmail.com>
2021-09-06 17:11:09 +0200

    XPP: Add API router

--- Node-JS/X-Press-Publishing/server.js
+++ Node-JS/X-Press-Publishing/server.js
@@ -4,11 +4,14 @@ const morgan = require("morgan");
 const errorHandler = require("errorhandler");
 const app = express();
 const PORT = process.env.PORT || 4001;
+const apiRouter = require("./api/api");
 
 app.use(express.json());
 app.use(errorHandler());
 app.use(cors());
 app.use(morgan("dev"));
+
+app.use("/api", apiRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running at port ${PORT}...`);
