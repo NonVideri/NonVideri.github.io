fd94e6651d101500630e174096450fc4414d2252
NonVideri <krzysztof.adam.moszynski@gmail.com>
2021-09-06 14:23:55 +0200

    Codestrips: Add strip posting

--- Node-JS/Codestrips/app.js
+++ Node-JS/Codestrips/app.js
@@ -17,6 +17,38 @@ app.get("/strips", (req, res) => {
   });
 });
 
+const validateStrip = (req, res, next) => {
+  const newStrip = req.body.strip;
+  if (!newStrip.head || !newStrip.body || !newStrip.background || !newStrip.bubbleType) {
+    return res.sendStatus(400);
+  }
+  next();
+};
+
+app.post("/strips", validateStrip, (req, res) => {
+  const newStrip = req.body.strip;
+  db.run(
+    `INSERT INTO Strip (head, body, background, bubble_type, bubble_text, caption)
+  VALUES ($head, $body, $background, $bubbleType, $bubbleText,
+    $caption)`,
+    {
+      $head: newStrip.head,
+      $body: newStrip.body,
+      $bubbleType: newStrip.bubbleType,
+      $background: newStrip.background,
+      $bubbleText: newStrip.bubbleText,
+      $caption: newStrip.caption
+    },
+    function (err) {
+      if (err) return res.sendStatus(500);
+      db.get(`SELECT * FROM Strip WHERE id = ${this.lastID}`, (err, row) => {
+        if (!row) return res.sendStatus(500);
+        res.status(201).send({ strip: row });
+      });
+    }
+  );
+});
+
 const PORT = process.env.PORT || 4001;
 
 app.listen(PORT, () => {
