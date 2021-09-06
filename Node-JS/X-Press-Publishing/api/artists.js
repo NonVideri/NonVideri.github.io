9f04f5f4171ec81ae4e52482ed0457e4a5134cca
NonVideri <krzysztof.adam.moszynski@gmail.com>
2021-09-06 18:46:28 +0200

    XPP: Add getting all artists

--- Node-JS/X-Press-Publishing/api/artists.js
+++ Node-JS/X-Press-Publishing/api/artists.js
@@ -3,4 +3,11 @@ const artistsRouter = express.Router();
 const sqlite3 = require("sqlite3");
 const db = new sqlite3.Database(process.env.TEST_DATABASE || "./database.sqlite");
 
+artistsRouter.get("/", (req, res, next) => {
+  db.all("SELECT * FROM Artist WHERE Artist.is_currently_employed = 1", (err, artists) => {
+    if (err) return next(err);
+    res.status(200).json({ artists });
+  });
+});
+
 module.exports = artistsRouter;
