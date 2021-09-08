deef36df6b7cea7610807fada6a7630ffd79046e
NonVideri <krzysztof.adam.moszynski@gmail.com>
2021-09-08 19:19:12 +0200

    XPP: Add creating series

--- Node-JS/X-Press-Publishing/api/series.js
+++ Node-JS/X-Press-Publishing/api/series.js
@@ -3,6 +3,12 @@ const seriesRouter = express.Router();
 const sqlite3 = require("sqlite3");
 const db = new sqlite3.Database(process.env.TEST_DATABASE || "./database.sqlite");
 
+const validateSeries = (req, res, next) => {
+  const newSeries = req.body.series;
+  if (!newSeries.name || !newSeries.description) return res.sendStatus(400);
+  else next();
+};
+
 seriesRouter.param("seriesId", (req, res, next, seriesId) => {
   db.get(`SELECT * FROM Series WHERE Series.id = ${seriesId}`, (err, series) => {
     if (err) return next(err);
@@ -21,6 +27,24 @@ seriesRouter.get("/", (req, res, next) => {
 
 seriesRouter.get("/:seriesId", (req, res) => {
   res.status(200).json({ series: req.series });
+});
+
+seriesRouter.post("/", validateSeries, (req, res) => {
+  const newSeries = req.body.series;
+  db.run(
+    `INSERT INTO Series (name, description)
+    VALUES ($name, $description)`,
+    {
+      $name: newSeries.name,
+      $description: newSeries.description
+    },
+    function (err) {
+      if (err) return next(err);
+      db.get(`SELECT * FROM Series WHERE Series.id = ${this.lastID}`, (err, series) => {
+        res.status(201).json({ series });
+      });
+    }
+  );
 });
 
 module.exports = seriesRouter;
