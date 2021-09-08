16af9925118522f53704b58791dec326fc30572b
NonVideri <krzysztof.adam.moszynski@gmail.com>
2021-09-08 22:51:51 +0200

    XPP: Add issues router

--- Node-JS/X-Press-Publishing/api/series.js
+++ Node-JS/X-Press-Publishing/api/series.js
@@ -2,6 +2,9 @@ const express = require("express");
 const seriesRouter = express.Router();
 const sqlite3 = require("sqlite3");
 const db = new sqlite3.Database(process.env.TEST_DATABASE || "./database.sqlite");
+const issuesRouter = require("./issues");
+
+seriesRouter.use("/:seriesId/issues", issuesRouter);
 
 const validateSeries = (req, res, next) => {
   const newSeries = req.body.series;
