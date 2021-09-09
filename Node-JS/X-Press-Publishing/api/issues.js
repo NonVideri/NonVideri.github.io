d1c307ebb6ef85f7487434694aba48f69ce453c8
NonVideri <krzysztof.adam.moszynski@gmail.com>
2021-09-09 14:10:04 +0200

    XPP: Add updating issues

--- Node-JS/X-Press-Publishing/api/issues.js
+++ Node-JS/X-Press-Publishing/api/issues.js
@@ -3,7 +3,7 @@ const issuesRouter = express.Router({ mergeParams: true });
 const sqlite3 = require("sqlite3");
 const db = new sqlite3.Database(process.env.TEST_DATABASE || "./database.sqlite");
 
-const validateIssues = (req, res, next) => {
+const validateIssue = (req, res, next) => {
   const issue = req.body.issue;
   if (!issue.name || !issue.issueNumber || !issue.publicationDate || !issue.artistId)
     return res.sendStatus(400);
@@ -19,18 +19,27 @@ const validateIssues = (req, res, next) => {
   next();
 };
 
+issuesRouter.param("issueId", (req, res, next, issueId) => {
+  db.get(`SELECT * FROM Issue WHERE Issue.id = ${issueId}`, (err, issue) => {
+    if (err) return next(err);
+    if (!series) return res.sendStatus(404);
+    req.issue = issue;
+    next();
+  });
+});
+
 issuesRouter.get("/", (req, res, next) => {
   db.all(
     "SELECT * FROM Issue WHERE Issue.series_id = $seriesId",
     { $issuesId: req.params.seriesId },
-    (err, issues) => {
+    (err, issue) => {
       if (err) return next(err);
-      res.status(200).json({ issues });
+      res.status(200).json({ issue });
     }
   );
 });
 
-issuesRouter.post("/", validateIssues, (req, res) => {
+issuesRouter.post("/", validateIssue, (req, res) => {
   const issue = req.body.issue;
   db.run(
     `INSERT INTO Issue (name, issue_number, publication_date, artist_id)
@@ -43,8 +52,28 @@ issuesRouter.post("/", validateIssues, (req, res) => {
     },
     function (err) {
       if (err) return next(err);
-      db.get(`SELECT * FROM Issue WHERE Issue.id = ${this.lastID}`, (err, issues) => {
-        res.status(201).json({ issues });
+      db.get(`SELECT * FROM Issue WHERE Issue.id = ${this.lastID}`, (err, issue) => {
+        res.status(201).json({ issue });
+      });
+    }
+  );
+});
+
+issuesRouter.put("/:issueId", validateIssue, (req, res) => {
+  const issue = req.body.issue;
+  db.run(
+    `UPDATE Issue SET
+    name = $name, issue_number = $issueNumber, publication_date = $publicationDate, artist_id = $artistId`,
+    {
+      $name: issue.name,
+      $issueNumber: issue.issueNumber,
+      $publicationDate: issue.publicationDate,
+      $artistId: issue.artistId
+    },
+    function (err) {
+      if (err) return next(err);
+      db.get(`SELECT * FROM Issue WHERE Issue.id = ${this.lastID}`, (err, issue) => {
+        res.status(201).json({ issue });
       });
     }
   );
