3cb797851c96b80ce9c296c6e9aed681be7af69f
NonVideri <krzysztof.adam.moszynski@gmail.com>
2021-09-13 14:52:03 +0200

    Nest Products: Add product updating

--- Node-JS/Nest-Products/src/products/products.service.ts
+++ Node-JS/Nest-Products/src/products/products.service.ts
@@ -5,6 +5,13 @@ import { Product } from './product.model';
 export class ProductsService {
   private products: Product[];
 
+  private findProduct(id: string): [Product, number] {
+    const productIndex = this.products.findIndex((prod) => prod.id == id);
+    const product = this.products[productIndex];
+    if (!product) throw new NotFoundException('Could not find product.');
+    return [product, productIndex];
+  }
+
   insertProduct(title: string, desc: string, price: number) {
     const prodId = new Date().toString();
     const newProduct = new Product(prodId, title, desc, price);
@@ -18,8 +25,15 @@ export class ProductsService {
   }
 
   getProduct(productId: string) {
-    const product = this.products.find((prod) => prod.id == productId);
-    if (!product) throw new NotFoundException('Could not find product.');
-    return { ...product };
+    return { ...this.findProduct(productId)[0] };
+  }
+
+  updateProduct(productId: string, title: string, desc: string, price: number) {
+    const [product, index] = this.findProduct(productId);
+    const updatedProduct = { ...product };
+    if (title) updatedProduct.title = title;
+    if (desc) updatedProduct.description = desc;
+    if (price) updatedProduct.price = price;
+    this.products[index] = updatedProduct;
   }
 }
