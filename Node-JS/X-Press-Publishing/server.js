4105effb4348112067f7cbd813b7f5e90fbbe681
NonVideri <krzysztof.adam.moszynski@gmail.com>
2021-09-15 20:04:13 +0200

    XPP: Refactor server

--- Node-JS/X-Press-Publishing/server.js
+++ Node-JS/X-Press-Publishing/server.js
@@ -1,19 +1,17 @@
-const express = require("express");
-const cors = require("cors");
-const morgan = require("morgan");
-const errorHandler = require("errorhandler");
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const errorHandler = require('errorhandler');
 const app = express();
 const PORT = process.env.PORT || 4001;
-const apiRouter = require("./api/api");
+const apiRouter = require('./api/api');
 
-app.use(express.static("public"));
-app.use(express.static("src"));
 app.use(express.json());
 app.use(errorHandler());
 app.use(cors());
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 
-app.use("/api", apiRouter);
+app.use('/api', apiRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running at port ${PORT}...`);
