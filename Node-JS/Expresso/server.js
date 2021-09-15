0a4ec22af4e26d0933d7f8ea29e36868a0365fee
NonVideri <krzysztof.adam.moszynski@gmail.com>
2021-09-15 20:05:06 +0200

    Expresso: Create base server

---
+++ Node-JS/Expresso/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const errorHandler = require('errorhandler');
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
+
+app.use(express.static('public'));
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(errorHandler());
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Server listening at port ${PORT}...`);
+});
+
+module.exports = app;
