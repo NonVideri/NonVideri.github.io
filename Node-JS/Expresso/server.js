ef219e6b582689007fa087b2dc9a66dc08bd7bb4
NonVideri <krzysztof.adam.moszynski@gmail.com>
2021-09-16 15:28:39 +0200

    Expresso: Create API router

--- Node-JS/Expresso/server.js
+++ Node-JS/Expresso/server.js
@@ -4,14 +4,16 @@ const morgan = require('morgan');
 const errorHandler = require('errorhandler');
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
+const apiRouter = require('./api/api');
 
 app.use(express.static('public'));
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(errorHandler());
 
-const PORT = process.env.PORT || 4000;
+app.use('/api', apiRouter);
 
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server listening at port ${PORT}...`);
 });
