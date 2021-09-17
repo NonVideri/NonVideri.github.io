32464223c208745c055ee1a3c79d3b6b7af7d51c
NonVideri <krzysztof.adam.moszynski@gmail.com>
2021-09-17 08:45:01 +0200

    Expresso: Add employee creation

--- Node-JS/Expresso/api/employees.js
+++ Node-JS/Expresso/api/employees.js
@@ -3,6 +3,12 @@ const sqlite3 = require('sqlite3');
 const employeesRouter = express.Router();
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
+const validateEmployee = (req, res, next) => {
+  const newEmployee = req.body.employee;
+  if (!newEmployee.name || !newEmployee.position || !newEmployee.wage) return res.sendStatus(400);
+  next();
+};
+
 employeesRouter.param('employeeId', (req, res, next, employeeId) => {
   db.get(`SELECT * FROM Employee WHERE Employee.id = ${employeeId}`, (err, employee) => {
     if (err) return next(err);
@@ -17,6 +23,25 @@ employeesRouter.get('/', (req, res, next) => {
     if (err) return next(err);
     res.status(200).json({ employees });
   });
+});
+
+employeesRouter.post('/', validateEmployee, (req, res, next) => {
+  const newEmployee = req.body.employee;
+  db.run(
+    `INSERT INTO Employee (name, position, wage)
+    VALUES ($name, $position, $wage)`,
+    {
+      $name: newEmployee.name,
+      $position: newEmployee.position,
+      $wage: newEmployee.wage
+    },
+    function (err) {
+      if (err) return next(err);
+      db.get(`SELECT * FROM Employee WHERE Employee.id = ${this.lastID}`, (err, employee) => {
+        res.status(201).json({ employee });
+      });
+    }
+  );
 });
 
 employeesRouter.get('/:employeeId', (req, res, next) => {
