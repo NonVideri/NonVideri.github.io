bb9971b19ae3ebd8bb7a9f4a0adc98b30768d80f
NonVideri <krzysztof.adam.moszynski@gmail.com>
2021-09-17 22:58:20 +0200

    Expresso: Add getting menu items

--- Node-JS/Expresso/api/menu-items.js
+++ Node-JS/Expresso/api/menu-items.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const sqlite3 = require('sqlite3');
-const menuItemsRouter = express.Router();
+const menuItemsRouter = express.Router({ mergeParams: true });
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
+
+menuItemsRouter.get('/', (req, res, next) => {
+  db.all(`SELECT * FROM MenuItem WHERE menu_id = ${req.params.id}`, (err, menuItems) => {
+    if (err) return next(err);
+    res.status(200).json({ menuItems });
+  });
+});
 
 module.exports = menuItemsRouter;
