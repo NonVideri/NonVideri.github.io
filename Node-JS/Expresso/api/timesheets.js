0e8eed05bf3f97f415eb813aacba74318c1a79c4
NonVideri <krzysztof.adam.moszynski@gmail.com>
2021-09-17 20:11:20 +0200

    Expresso: Add timesheet updating, optimize timesheet creation

--- Node-JS/Expresso/api/timesheets.js
+++ Node-JS/Expresso/api/timesheets.js
@@ -9,6 +9,15 @@ const validateTimesheet = (req, res, next) => {
   next();
 };
 
+timesheetsRouter.param('timesheetId', (req, res, next, id) => {
+  db.get(`SELECT * FROM Timesheet WHERE Timesheet.id = ${id}`, (err, timesheet) => {
+    if (err) return next(err);
+    if (!timesheet) return res.sendStatus(404);
+    req.timesheet = timesheet;
+    next();
+  });
+});
+
 timesheetsRouter.get('/', (req, res, next) => {
   db.all(`SELECT * FROM Timesheet WHERE employee_id = ${req.params.id}`, (err, timesheets) => {
     if (err) return next(err);
@@ -29,10 +38,32 @@ timesheetsRouter.post('/', validateTimesheet, (req, res, next) => {
     },
     function (err) {
       if (err) return next(err);
-      db.get(`SELECT * FROM Timesheet WHERE id = ${this.lastID}`, (err, timesheet) => {
-        if (err) return next(err);
-        res.status(201).json({ timesheet });
-      });
+      const timesheet = {
+        id: this.lastID,
+        hours: newTimesheet.hours,
+        rate: newTimesheet.rate,
+        date: newTimesheet.date,
+        employee_id: Number(req.params.id)
+      };
+      res.status(201).json({ timesheet });
+    }
+  );
+});
+
+timesheetsRouter.put('/:timesheetId', validateTimesheet, (req, res, next) => {
+  const newTimesheet = req.body.timesheet;
+  db.run(
+    `UPDATE Timesheet SET hours = $hours, rate = $rate, date = $date
+  WHERE Timesheet.id = ${req.params.timesheetId}`,
+    { $hours: newTimesheet.hours, $rate: newTimesheet.rate, $date: newTimesheet.date },
+    (err) => {
+      if (err) return next(err);
+      [req.timesheet.hours, req.timesheet.rate, req.timesheet.date] = [
+        newTimesheet.hours,
+        newTimesheet.rate,
+        newTimesheet.date
+      ];
+      res.status(200).json({ timesheet: req.timesheet });
     }
   );
 });
