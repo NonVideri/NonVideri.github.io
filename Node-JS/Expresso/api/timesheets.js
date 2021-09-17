7219fa207413e9dbb3aab167adf649cc8b78cb7a
NonVideri <krzysztof.adam.moszynski@gmail.com>
2021-09-17 19:38:53 +0200

    Expresso: Add timesheets getting

--- Node-JS/Expresso/api/timesheets.js
+++ Node-JS/Expresso/api/timesheets.js
@@ -3,4 +3,11 @@ const sqlite3 = require('sqlite3');
 const timesheetsRouter = express.Router({ mergeParams: true });
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
+timesheetsRouter.get('/', (req, res, next) => {
+  db.all(`SELECT * FROM Timesheet WHERE employee_id = ${req.params.id}`, (err, timesheets) => {
+    if (err) return next(err);
+    res.status(200).json({ timesheets });
+  });
+});
+
 module.exports = timesheetsRouter;
