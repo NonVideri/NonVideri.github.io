ac5defdd2eaae5448865cfe23a355993bec85cbb
NonVideri <krzysztof.adam.moszynski@gmail.com>
2021-09-18 12:03:15 +0200

    Expresso: Add Helmet security middleware

--- Node-JS/Expresso/server.js
+++ Node-JS/Expresso/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const errorHandler = require('errorhandler');
+const helmet = require('helmet');
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 const apiRouter = require('./api/api');
@@ -10,6 +11,7 @@ app.use(express.static('public'));
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(errorHandler());
+app.use(helmet());
 
 app.use('/api', apiRouter);
 
