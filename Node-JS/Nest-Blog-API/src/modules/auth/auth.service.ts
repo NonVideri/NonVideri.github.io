9bb75a8a245c8e1a80d8072c218305731f718b81
NonVideri <krzysztof.adam.moszynski@gmail.com>
2021-09-19 22:35:56 +0200

    NBA: Implement local Passport strategy

--- Node-JS/Nest-Blog-API/src/modules/auth/auth.service.ts
+++ Node-JS/Nest-Blog-API/src/modules/auth/auth.service.ts
@@ -1,4 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { UsersService } from '../users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly userService: UsersService) {}
+
+  async validateUser(username: string, pass: string) {
+    // find if user with this email exists
+    const user = await this.userService.findOneByEmail(username);
+    if (!user) {
+      return null;
+    }
+
+    // find if the user matches with password
+    const match = await this.comparePassword(pass, user.password);
+    if (!match) {
+      return null;
+    }
+
+    // tslint:disable-next-line: no-string-literal
+    const { password, ...result } = user['dataValues'];
+    return result;
+  }
+
+  private async comparePassword(enteredPassword, dbPassword) {
+    const match = await bcrypt.compare(enteredPassword, dbPassword);
+    return match;
+  }
+}
