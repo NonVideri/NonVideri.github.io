e1db82ec0b449f0fa423d9d06c51b6216c7fb7ed
NonVideri <krzysztof.adam.moszynski@gmail.com>
2021-09-20 22:09:36 +0200

    NBA: Add user input validation

--- Node-JS/Nest-Blog-API/src/modules/users/dto/user.dto.ts
+++ Node-JS/Nest-Blog-API/src/modules/users/dto/user.dto.ts
@@ -1,8 +1,25 @@
-import { Model } from 'sequelize-typescript';
+import { IsNotEmpty, MinLength, IsEmail, IsEnum } from 'class-validator';
 
-export class UserDto extends Model {
+enum Gender {
+  MALE = 'male',
+  FEMALE = 'female',
+}
+
+export class UserDto {
+  @IsNotEmpty()
   readonly name: string;
+
+  @IsNotEmpty()
+  @IsEmail()
   readonly email: string;
+
+  @IsNotEmpty()
+  @MinLength(6)
   readonly password: string;
-  readonly gender: string;
+
+  @IsNotEmpty()
+  @IsEnum(Gender, {
+    message: 'gender must be either male or female',
+  })
+  readonly gender: Gender;
 }
