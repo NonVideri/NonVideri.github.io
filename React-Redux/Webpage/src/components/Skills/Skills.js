32c0a8039374b97d01a52f57f00f12931000d8f7
NonVideri <krzysztof.adam.moszynski@gmail.com>
2021-09-25 20:26:16 +0200

    Webpage: Update CV and Skills section

--- React-Redux/Webpage/src/components/Skills/Skills.js
+++ React-Redux/Webpage/src/components/Skills/Skills.js
@@ -5,38 +5,36 @@ export default function Skills() {
         <h2 className="section-title">Skills</h2>
         <div className="row">
           <article className="col-sm-4">
-            <h3 className="feature-title">Coding</h3>
             <ul className="feature-description">
-              <li>Node.js, Express, NestJS, mongoose</li>
+              <li>Node.js, Express, NestJS</li>
+              <li>mongoose, sqlite3, Sequelize, Passport</li>
+              <li>SQL (PostgreSQL, SQLite), MongoDB</li>
+              <li>HTTP, REST</li>
               <li>React, Redux</li>
               <li>JavaScript (ES6), TypeScript</li>
-              <li>Jest, RTL</li>
-              <li>SQL (PostgreSQL, SQLite), MongoDB</li>
-              <li>HTML5, CSS3, Bootstrap, Sass</li>
-              <li>Python</li>
-              <li>Git</li>
+              <li>Jest, RTL, Mocha/Chai, Manual Testing</li>
             </ul>
           </article>
           <div className="divider"></div>
           <article className="col-sm-4">
-            <h3 className="feature-title">Other</h3>
             <ul className="feature-description">
-              <li>Mnemonics, mind maps, fast learning</li>
-              <li>Speed reading</li>
-              <li>Time management (Eisenhower matrix, pomodoro)</li>
-              <li>Scrum (Trello)</li>
+              <li>HTML5, CSS3, Bootstrap, Sass</li>
+              <li>Python</li>
+              <li>Git</li>
+              <li>Mnemonics (fast learning, mind maps), speed reading</li>
+              <li>Time management</li>
+              <li>Teamwork, Scrum (Trello)</li>
               <li>MS Office, Photoshop, Premiere Pro</li>
             </ul>
           </article>
           <div className="divider"></div>
           <article className="col-sm-4">
-            <h3 className="feature-title">Experience</h3>
             <ul className="feature-description">
-              <li>Wordpress</li>
-              <li>Specialized interpretation, translation</li>
+              <li>Application Testing</li>
+              <li>Wordpress Webpage Management, Improvement</li>
               <li>Project team management (15 people)</li>
-              <li>Public speaking, training, mentoring</li>
-              <li>Customer contact</li>
+              <li>Public speaking, mentoring</li>
+              <li>Specialized interpretation, translation</li>
               <li>NGO external relations</li>
             </ul>
           </article>
