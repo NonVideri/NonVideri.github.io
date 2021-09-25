e63808889e432409a420a6064a961c5f83c1dacb
NonVideri <krzysztof.adam.moszynski@gmail.com>
2021-09-25 21:32:58 +0200

    XPP: Implement server security

--- Node-JS/X-Press-Publishing/server.js
+++ Node-JS/X-Press-Publishing/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const errorHandler = require('errorhandler');
+const helmet = require('helmet');
 const app = express();
 const apiRouter = require('./api/api');
 
@@ -9,6 +10,7 @@ app.use(express.json());
 app.use(errorHandler());
 app.use(cors());
 app.use(morgan('dev'));
+app.use(helmet());
 
 app.use('/api', apiRouter);
 
