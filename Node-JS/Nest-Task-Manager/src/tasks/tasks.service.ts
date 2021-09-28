43bbd0967e9cc53cad437957de8b62d723462d0c
NonVideri <krzysztof.adam.moszynski@gmail.com>
2021-09-28 18:08:06 +0200

    NTM: Add exception when trying to get non-existing task

--- Node-JS/Nest-Task-Manager/src/tasks/tasks.service.ts
+++ Node-JS/Nest-Task-Manager/src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import * as uuid from 'uuid';
 import { CreateTaskDto, GetTasksFilterDto } from './tasks.dtos';
@@ -26,7 +26,10 @@ export class TasksService {
   }
 
   getTaskById(id: string): Task {
-    return this.tasks.find((task) => task.id === id);
+    const found = this.tasks.find((task) => task.id === id);
+
+    if (!found) throw new NotFoundException(`Task with ID "${id}" not found.`);
+    return found;
   }
 
   createTask(createTaskDto: CreateTaskDto): Task {
@@ -44,7 +47,9 @@ export class TasksService {
   }
 
   deleteTask(id: string): void {
-    this.tasks = this.tasks.filter((task) => task.id !== id);
+    const found = this.getTaskById(id);
+    // Splice could be faster here once I figure out how to do it the DRY way with getTaskById()
+    this.tasks = this.tasks.filter((task) => task.id !== found.id);
   }
 
   updateTaskStatus(id: string, status: TaskStatus): Task {
