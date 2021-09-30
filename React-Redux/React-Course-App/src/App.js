212b8a3addb59537e912765aa20ae16edfe8555b
NonVideri <krzysztof.adam.moszynski@gmail.com>
2021-09-30 10:49:24 +0200

    RCA: Add task toggling

--- React-Redux/React-Course-App/src/App.js
+++ React-Redux/React-Course-App/src/App.js
@@ -1,4 +1,19 @@
+import './App.css';
 import { useState } from 'react';
+
+function ToDoItem(props) {
+  const [done, setDone] = useState(false);
+
+  const toggleDone = () => {
+    setDone(!done);
+  };
+
+  return (
+    <div onClick={toggleDone} className={done ? 'doneTodo' : ''}>
+      <p>{props.task}</p>
+    </div>
+  );
+}
 
 function ToDoList(props) {
   const [tasks, setTasks] = useState(props.tasks);
@@ -17,9 +32,7 @@ function ToDoList(props) {
     <>
       <h1>{props.title}</h1>
       {tasks.map((task) => (
-        <div>
-          <p>{task}</p>
-        </div>
+        <ToDoItem task={task} />
       ))}
       <input type="text" onChange={updateInput} value={input} />
       <button onClick={addTask}>Add task</button>
