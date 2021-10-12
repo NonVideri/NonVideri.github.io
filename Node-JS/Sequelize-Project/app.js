42a4ebc8620cb5668fd4e1352c9d1cd5dd750855
NonVideri <krzysztof.adam.moszynski@gmail.com>
2021-10-12 21:49:57 +0200

    Sequelize Project: Create basic Express server

--- Node-JS/Sequelize-Project/app.js
+++ Node-JS/Sequelize-Project/app.js
@@ -1,7 +1,24 @@
-const { sequelize } = require('./models');
+const express = require('express');
+const app = express();
+const { sequelize, User } = require('./models');
+const PORT = 5000;
 
-const main = async () => {
-  await sequelize.sync();
-};
+app.use(express.json());
 
-main();
+app.post('/users', async (req, res) => {
+  const { name, email, role } = req.body;
+
+  try {
+    const user = await User.create({ name, email, role });
+    return res.json(user);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json(err);
+  }
+});
+
+app.listen(PORT, async () => {
+  console.log(`Server up on http://localhost:${PORT}.`);
+  await sequelize.sync({ force: true });
+  console.log('Database synced.');
+});
