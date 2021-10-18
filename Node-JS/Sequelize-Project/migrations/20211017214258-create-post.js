ee8996cd0907c289f0c78564a13a31da5a818fe3
NonVideri <krzysztof.adam.moszynski@gmail.com>
2021-10-18 11:33:22 +0200

    Sequelize Project: Define User-Post associations

--- Node-JS/Sequelize-Project/migrations/20211017214258-create-post.js
+++ Node-JS/Sequelize-Project/migrations/20211017214258-create-post.js
@@ -4,6 +4,7 @@ module.exports = {
     await queryInterface.createTable('Posts', {
       id: { type: DataTypes.INTEGER, allowNull: false, autoIncrement: true, primaryKey: true },
       uuid: { type: DataTypes.UUID, defaultValue: DataTypes.UUIDV4 },
+      userId: { type: DataTypes.INTEGER, allowNull: false },
       body: { type: DataTypes.STRING, allowNull: false },
       createdAt: { type: DataTypes.DATE, allowNull: false },
       updatedAt: { type: DataTypes.DATE, allowNull: false }
