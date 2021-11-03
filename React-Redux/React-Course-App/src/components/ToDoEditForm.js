4f1af65a56de4839367f4d5441db00c366a5577e
NonVideri <krzysztof.adam.moszynski@gmail.com>
2021-11-03 11:12:38 +0100

    RCA: Add basic input validation

--- React-Redux/React-Course-App/src/components/ToDoEditForm.js
+++ React-Redux/React-Course-App/src/components/ToDoEditForm.js
@@ -1,7 +1,7 @@
 import { Formik } from 'formik';
 import { useEffect, useState } from 'react';
 import { get, update } from '../helpers/tasksApi';
-import { SubmitButton, TextInput, Label, Select } from '../helpers/theme';
+import { SubmitButton, TextInput, Label, Select, ErrorMsg } from '../helpers/theme';
 
 export default function ToDoEditForm(props) {
   const [fetched, setFetched] = useState(false);
@@ -22,10 +22,17 @@ export default function ToDoEditForm(props) {
           initialValues={{ ...item }}
           onSubmit={(values) => {
             update(itemId, { ...values });
+          }}
+          validate={(values) => {
+            let errors = {};
+            if (!values.content) errors.content = 'Required.';
+            else if (values.content.length < 3) errors.content = 'Too short. Minimum 3 characters.';
+            return errors;
           }}>
           {({ values, errors, touched, handleBlur, handleChange, handleSubmit, isSubmitting }) => (
             <form onSubmit={handleSubmit}>
               <Label>
+                Content *<ErrorMsg>{errors.content}</ErrorMsg>
                 <TextInput name="content" onChange={handleChange} value={values.content} />
               </Label>
 
