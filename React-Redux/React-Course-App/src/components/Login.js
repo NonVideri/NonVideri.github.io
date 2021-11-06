1e1553fe8fe923f14e6dbf5042a5b36dcd33cf8c
NonVideri <krzysztof.adam.moszynski@gmail.com>
2021-11-06 23:56:34 +0100

    RCA: Add basic FB login, user greeting

--- React-Redux/React-Course-App/src/components/Login.js
+++ React-Redux/React-Course-App/src/components/Login.js
@@ -3,14 +3,25 @@ import { SubmitButton } from '../helpers/theme';
 
 export default function Login(props) {
   const [processing, setProcessing] = useState(false);
+  const [currentUser, setCurrentUser] = useState(null);
 
   const fbLogin = () => {
     setProcessing(true);
+    window.FB.getLoginStatus((response) => {
+      console.log(response);
+      if (response.status !== 'connected') window.FB.login();
+      else
+        window.FB.api('/me', (user) => {
+          console.log(user);
+          setProcessing(false);
+          setCurrentUser(user);
+        });
+    });
   };
 
   return (
     <div>
-      <p>You must login to view page.</p>
+      {currentUser ? <div>Hello, {currentUser.name}!</div> : <p>You must login to view page.</p>}
       {processing ? (
         <div>Authenticating...</div>
       ) : (
