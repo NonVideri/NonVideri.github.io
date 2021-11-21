227617c3847c14e330777614e4d1df5f49ef8f1b
NonVideri <krzysztof.adam.moszynski@gmail.com>
2021-11-21 22:10:34 +0100

    RCA: Add context to login

--- React-Redux/React-Course-App/src/components/Login.js
+++ React-Redux/React-Course-App/src/components/Login.js
@@ -1,41 +1,25 @@
 import { Redirect } from 'react-router';
 import { useState } from 'react/cjs/react.development';
+import { CurrentUserConsumer } from '../context/CurrentUser';
 import { SubmitButton } from '../helpers/theme';
 
 export default function Login(props) {
-  const [processing, setProcessing] = useState(false);
-  const [currentUser, setCurrentUser] = useState(null);
-  const [finished, setFinished] = useState(false);
-
-  const fbLogin = () => {
-    setProcessing(true);
-    window.FB.getLoginStatus((response) => {
-      if (response.status !== 'connected') window.FB.login();
-      else
-        window.FB.api('/me', (user) => {
-          sessionStorage.setItem('currentUser', user);
-          setProcessing(false);
-          setFinished(true);
-          setCurrentUser(user);
-        });
-    });
-  };
-
   const { from } = props.location.state || { from: { pathname: '/' } };
 
-  if (finished) return <Redirect to={from} />;
   return (
-    <div>
-      {currentUser ? (
-        <div>Hello, {currentUser.name}!</div>
-      ) : (
-        <p>You must login to view page {from.pathname}.</p>
-      )}
-      {processing ? (
-        <div>Authenticating...</div>
-      ) : (
-        <SubmitButton onClick={fbLogin}>Log in with Facebook</SubmitButton>
-      )}
-    </div>
+    <CurrentUserConsumer>
+      {({ user, login, processing }) => {
+        <div>
+          {user && <Redirect to={from} />}
+          <p>You must login to view page {from.pathname}.</p>
+
+          {processing ? (
+            <div>Authenticating...</div>
+          ) : (
+            <SubmitButton onClick={login}>Log in with Facebook</SubmitButton>
+          )}
+        </div>;
+      }}
+    </CurrentUserConsumer>
   );
 }
