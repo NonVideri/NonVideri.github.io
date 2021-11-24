67e78ba0285d8c9148b51287f6233ce323acf486
NonVideri <krzysztof.adam.moszynski@gmail.com>
2021-11-24 11:13:33 +0100

    SAPI: Create basic app structure

--- Node-JS/Store-API/app.js
+++ Node-JS/Store-API/app.js
@@ -1,1 +1,34 @@
-console.log('04 Store API')
+require('dotenv').config();
+// async errors
+const express = require('express');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+const notFoundMiddleware = require('./middleware/not-found');
+const app = express();
+
+// middleware
+app.use(express.json());
+
+// routes
+app.get('/', (req, res) => {
+  res.send('<h1>Store API</h1><a href="/api/v1/products">Products Route</a>');
+});
+
+// Products route
+
+// error handling
+app.use(errorHandlerMiddleware);
+app.use(notFoundMiddleware);
+
+// launch app
+const PORT = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    // connect to DB
+    app.listen(PORT, console.log(`Server is listening at port ${PORT}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
