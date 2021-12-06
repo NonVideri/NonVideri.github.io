6988a8789a1859cc27b96bfa8675c8b9160f5eef
NonVideri <krzysztof.adam.moszynski@gmail.com>
2021-12-06 23:09:22 +0100

    JWTP: Add JWT signing

--- Node-JS/JWT-Project/controllers/main.js
+++ Node-JS/JWT-Project/controllers/main.js
@@ -2,14 +2,18 @@
 // if exist and match create new JWT
 // send back to front-end
 // setup authentication so only the request with JWT can access the dashboard
-
+const jwt = require('jsonwebtoken');
 const CustomAPIError = require('../errors/custom-error');
 
 const login = async (req, res) => {
   const { username, password } = req.body;
   if (!username || !password)
     throw new CustomAPIError('You need to provide email and password.', 400);
-  res.send('Fake Login/Register/Signup');
+  // Dummy data
+  const id = new Date().getDate();
+
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, { expiresIn: '30d' });
+  res.status(200).json({ msg: 'User created.', token });
 };
 
 const dashboard = async (req, res) => {
