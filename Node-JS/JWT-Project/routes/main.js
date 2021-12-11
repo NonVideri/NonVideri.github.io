bf6c6b4752f8088b3eb3808236af812a713182c3
NonVideri <krzysztof.adam.moszynski@gmail.com>
2021-12-11 23:45:28 +0100

    JWTP: Add JWT authorization

--- Node-JS/JWT-Project/routes/main.js
+++ Node-JS/JWT-Project/routes/main.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const { dashboard, login } = require('../controllers/main');
+const authMiddleware = require('../middleware/auth');
 const mainRouter = express.Router();
 
-mainRouter.route('/dashboard').get(dashboard);
+mainRouter.route('/dashboard').get(authMiddleware, dashboard);
 mainRouter.route('/login').post(login);
 
 module.exports = mainRouter;
