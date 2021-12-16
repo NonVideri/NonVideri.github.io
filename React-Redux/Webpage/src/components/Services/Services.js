3fdd026b9cfa0f900d12aed11fb511a49d3c9656
NonVideri <krzysztof.adam.moszynski@gmail.com>
2021-12-16 23:13:29 +0100

    Webpage: Add Services table

--- React-Redux/Webpage/src/components/Services/Services.js
+++ React-Redux/Webpage/src/components/Services/Services.js
@@ -1,4 +1,50 @@
+import React from 'react';
+import { useTable } from 'react-table';
+
+const data = React.useMemo(() => [
+  {
+    service: 'Written Japanese translations',
+    unit: '1 standard page*',
+    pricePLN: '100 PLN',
+    priceUSD: '25 USD',
+    priceBTC: 'multi'
+  },
+  {
+    service: 'Written Polish-English translations',
+    unit: '1 standard page*',
+    pricePLN: '50 PLN',
+    priceUSD: '12.5 USD',
+    priceBTC: 'multi'
+  },
+  {
+    service: 'Japanese interpretation**',
+    unit: '4 hours',
+    pricePLN: '750 PLN',
+    priceUSD: '187 USD',
+    priceBTC: 'multi'
+  },
+  {
+    service: 'Polish-English interpretation**',
+    unit: '4 hours',
+    pricePLN: '500 PLN',
+    priceUSD: '125 USD',
+    priceBTC: 'multi'
+  }
+]);
+
+const columns = React.useMemo(() => [
+  { Header: 'Service', accessor: 'service' },
+  { Header: 'Billing unit', accessor: 'unit' },
+  { Header: 'Price in PLN', accessor: 'pricePLN' },
+  { Header: 'Price in USD', accessor: 'priceUSD' },
+  { Header: 'Price in BTC', accessor: 'priceBTC' }
+]);
+
 export default function Services() {
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({
+    columns,
+    data
+  });
   // if /translator
   return (
     <section className="services" id="services">
@@ -13,6 +59,37 @@ export default function Services() {
           // Tu React Table?
           // arkusz 1800 znaków w angielskim/polskim — 100 PLN / 25 USD / Price in USD exchanged with current rate
         }
+        <table {...getTableProps()}>
+          <thead>
+            {headerGroups.map(headerGroup => (
+              <tr {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map(column => (
+                  <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody {...getTableBodyProps()}>
+            {rows.map((row, i) => {
+              prepareRow(row);
+              return (
+                <tr {...row.getRowProps()}>
+                  {row.cells.map(cell => {
+                    return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>;
+                  })}
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+        <div className="section-intro">
+          * A standard page is 1800 characters in either English or Polish, depending on the
+          language used, including spaces.
+        </div>
+        <div className="section-intro">
+          ** In case of interpretation, the Customer is expected to cover possible travel (from
+          Warsaw) and lodging expenses.
+        </div>
       </div>
     </section>
   );
