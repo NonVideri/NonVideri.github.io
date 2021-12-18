38e66cef445bbff0c75b2f9bd3ad34e22fc67da7
NonVideri <krzysztof.adam.moszynski@gmail.com>
2021-12-18 16:48:06 +0100

    AFH: Add header inserting

--- Node-JS/Add-Filename-Headers/main.js
+++ Node-JS/Add-Filename-Headers/main.js
@@ -1,16 +1,29 @@
+const path = require('path');
 const fs = require('fs');
+const insertLine = require('insert-line');
 require('dotenv').config();
 const folderPath = process.env.FOLDER_PATH;
 
-const insertHeader = (path) => {};
+const insertHeader = (filePath) => {
+  const headerText = path.basename(filePath, '.md');
+  const header = `# ${headerText.slice(0, 1).toUpperCase()}${headerText.slice(1)}`;
+  insertLine(filePath)
+    .content(header)
+    .at(2)
+    .then((err) => {
+      if (err) throw new Error(err);
+    });
+};
 
-const digForMdFiles = (path) => {
-  fs.readdir(path, (error, files) => {
+const digForMdFiles = (filePath) => {
+  fs.readdir(filePath, (error, files) => {
     if (error) return console.log('Error', error);
 
     files.forEach((file) => {
-      if (file.endsWith('.md')) return insertHeader(`${path}/${file}`);
-      else if (!file.includes('.')) digForMdFiles(`${path}/${file}`);
+      if (file.endsWith('.md')) return insertHeader(`${filePath}/${file}`);
+      else if (!file.includes('.')) digForMdFiles(`${filePath}/${file}`);
     });
   });
 };
+
+digForMdFiles(folderPath);
