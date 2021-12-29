d710d94ff9544245fc3fdcc7e89e63717a8c46d8
NonVideri <krzysztof.adam.moszynski@gmail.com>
2021-12-29 17:50:23 +0100

    JAPI: Create a basic register controller

--- Node-JS/Jobs-API/controllers/auth.js
+++ Node-JS/Jobs-API/controllers/auth.js
@@ -1,5 +1,14 @@
+const { StatusCodes } = require('http-status-codes');
+const User = require('../models/User');
+const { BadRequestError } = require('../errors');
+
 const register = async (req, res) => {
-  res.send('Register user');
+  // Optional setting a check in the controller, can also use mongoose for validation
+  const { name, email, password } = req.body;
+  if (!name || !email || !password)
+    throw new BadRequestError('Please provide name, email and password.');
+  const user = await User.create({ ...req.body });
+  res.status(StatusCodes.CREATED).json({ user });
 };
 
 const login = async (req, res) => {
