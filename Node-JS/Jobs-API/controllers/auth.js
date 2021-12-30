8ee18ec0019dae3c22fc59600c71d3a3fa4e51d7
NonVideri <krzysztof.adam.moszynski@gmail.com>
2021-12-30 16:33:52 +0100

    JAPI: Secure the registration using bcrypt

--- Node-JS/Jobs-API/controllers/auth.js
+++ Node-JS/Jobs-API/controllers/auth.js
@@ -1,13 +1,22 @@
 const { StatusCodes } = require('http-status-codes');
+const bcrypt = require('bcryptjs');
+
 const User = require('../models/User');
 const { BadRequestError } = require('../errors');
 
 const register = async (req, res) => {
-  // Optional setting a check in the controller, can also use mongoose for validation
   const { name, email, password } = req.body;
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const tempUser = { name, email, password: hashedPassword };
+
+  // Optional setting a check in the controller, can also use mongoose for validation
   if (!name || !email || !password)
     throw new BadRequestError('Please provide name, email and password.');
-  const user = await User.create({ ...req.body });
+
+  const user = await User.create({ ...tempUser });
   res.status(StatusCodes.CREATED).json({ user });
 };
 
