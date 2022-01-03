b8d1d24e6253410ba8d8c833c6756123b069adf9
NonVideri <krzysztof.adam.moszynski@gmail.com>
2022-01-03 17:35:00 +0100

    JAPI: Create a password-hashing Mongoose middleware

--- Node-JS/Jobs-API/controllers/auth.js
+++ Node-JS/Jobs-API/controllers/auth.js
@@ -5,18 +5,11 @@ const User = require('../models/User');
 const { BadRequestError } = require('../errors');
 
 const register = async (req, res) => {
-  const { name, email, password } = req.body;
-
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(password, salt);
-
-  const tempUser = { name, email, password: hashedPassword };
-
   // Optional setting a check in the controller, can also use mongoose for validation
   if (!name || !email || !password)
     throw new BadRequestError('Please provide name, email and password.');
 
-  const user = await User.create({ ...tempUser });
+  const user = await User.create({ ...req.body });
   res.status(StatusCodes.CREATED).json({ user });
 };
 
