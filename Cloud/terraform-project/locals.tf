7b0d5f86efc4ff4cd891f7261befa39c87a282ce
NonVideri <krzysztof.adam.moszynski@gmail.com>
2022-06-24 23:33:56 +0200

    TFP: Add random integer provider

--- Cloud/terraform-project/locals.tf
+++ Cloud/terraform-project/locals.tf
@@ -1,3 +1,8 @@
+resource "random_integer" "rand" {
+  min = 10000
+  max = 99999
+}
+
 locals {
   common_tags = {
     company = var.company
--- Cloud/terraform-project/providers.tf
+++ Cloud/terraform-project/providers.tf
@@ -4,5 +4,14 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 3.0"
     }
+
+    random = {
+      source = "hashicorp/random"
+      version = "~> 3.0"
+    }
   }
+}
+
+provider "aws" {
+  region     = var.aws_region
 }
