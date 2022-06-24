527fa84f7515d39f9ff90d8e859102256c0ff4fd
NonVideri <krzysztof.adam.moszynski@gmail.com>
2022-06-24 03:40:57 +0200

    Add the second subnet and instance

--- Cloud/terraform-project/variables.tf
+++ Cloud/terraform-project/variables.tf
@@ -22,10 +22,10 @@ variable "vpc_cidr_block" {
   default = "10.0.0.0/16"
 }
 
-variable "subnet1_cidr_block" {
-  type = string
-  description = "CIDR Block for Subnet1 in VPC"
-  default = "10.0.0.0/24"
+variable "vpc_subnets_cidr_block" {
+  type = list(string)
+  description = "CIDR Block for Subnets in VPC"
+  default = ["10.0.0.0/24", "10.0.1.0/24"]
 }
 
 variable "enable_dns_hostnames" {
