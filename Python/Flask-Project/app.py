b80408dadf5e3e469983da2686e70268e8517253
NonVideri <krzysztof.adam.moszynski@gmail.com>
2022-06-29 20:00:49 +0200

    Switch form method to POST

--- Python/Flask-Project/app.py
+++ Python/Flask-Project/app.py
@@ -1,3 +1,4 @@
+from crypt import methods
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -7,7 +8,7 @@ def index():
   return render_template("index.html")
 
 
-@app.route("/greet")
+@app.route("/greet", methods=["POST"])
 def greet():
-  name = request.args.get("name")
+  name = request.body.get("name")
   return render_template("greet.html", name=name)
