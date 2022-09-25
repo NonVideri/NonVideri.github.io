ef0edf85afef7a8696bb43610573cea1a6fd5fad
NonVideri <krzysztof.adam.moszynski@gmail.com>
2022-09-25 13:51:04 +0200

    NB: Add global styling and basic blog content

--- Frontend/Nextjs-Blog/.next/static/chunks/webpack.js
+++ Frontend/Nextjs-Blog/.next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "40b0e5de8ebaa16d"; }
+/******/ 		__webpack_require__.h = function() { return "9e086024989cefb4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
