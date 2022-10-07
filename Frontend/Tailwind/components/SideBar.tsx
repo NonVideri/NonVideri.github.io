1c13af60ca692bcd197b32c8076b71249b4e9d67
NonVideri <krzysztof.adam.moszynski@gmail.com>
2022-10-07 20:56:25 +0200

    TW: Add icons from react-icons

--- Frontend/Tailwind/components/SideBar.tsx
+++ Frontend/Tailwind/components/SideBar.tsx
@@ -1,11 +1,19 @@
+import React from 'react';
+import { BsPlus, BsFillLightningFill, BsGearFill } from 'react-icons/bs';
+import { FaFire, FaPoo } from 'react-icons/fa';
+
 export default function Sidebar() {
   return (
     <div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col shadow-lg bg-primary text-secondary">
-      <i>A</i>
-      <i>B</i>
-      <i>C</i>
-      <i>D</i>
-      <i>E</i>
+      <SideBarIcon icon={<FaFire size="20" />} />
+      <SideBarIcon icon={<BsPlus size="20" />} />
+      <SideBarIcon icon={<BsFillLightningFill size="20" />} />
+      <SideBarIcon icon={<FaPoo size="20" />} />
+      <SideBarIcon icon={<BsGearFill size="20" />} />
     </div>
   );
+}
+
+function SideBarIcon({ icon }: { icon: React.ReactNode }) {
+  return <div className="sidebar-icon">{icon}</div>;
 }
