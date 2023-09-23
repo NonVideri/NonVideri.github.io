df02924cb669eaf122b24d9297539f7b1d074465
NonVideri <krzysztof.adam.moszynski@gmail.com>
2023-09-23 16:40:27 +0200

    Create basic page structure

---
+++ frontend/nextjs-todo/src/app/new/page.tsx
@@ -0,0 +1,7 @@
+export default function NewPage() {
+  return (
+    <>
+      <h1>New todo</h1>
+    </>
+  );
+}
--- frontend/nextjs-todo/src/app/page.tsx
+++ frontend/nextjs-todo/src/app/page.tsx
@@ -1,3 +1,17 @@
-export default function HomePage() {
-  return <h1>Home</h1>
+import Link from "next/link";
+
+export default async function HomePage() {
+  return (
+    <>
+      <header className="flex justify-between items-center mb-4">
+        <h1 className="font-2xl">Todos</h1>
+        <Link
+          className="border border-gray-300 text-gray-300 px-2 py-1 rounded hover:bg-gray-700 focus-within:bg-gray-700 outline-none"
+          href="/new">
+          New
+        </Link>
+      </header>
+      <ul className="pl-4"></ul>
+    </>
+  );
 }
