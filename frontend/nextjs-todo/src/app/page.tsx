baa1164ff3dc76606956c33cf2417e8243539754
NonVideri <krzysztof.adam.moszynski@gmail.com>
2023-09-23 17:23:13 +0200

    Add updating todoitem state

--- frontend/nextjs-todo/src/app/page.tsx
+++ frontend/nextjs-todo/src/app/page.tsx
@@ -6,6 +6,15 @@ function getTodos() {
   return prisma.todo.findMany();
 }
 
+async function toggleTodo(id: string, complete: boolean) {
+  "use server";
+
+  await prisma.todo.update({
+    where: { id },
+    data: { complete }
+  });
+}
+
 export default async function HomePage() {
   const todos = await getTodos();
 
@@ -21,7 +30,7 @@ export default async function HomePage() {
       </header>
       <ul className="pl-4">
         {todos.map((todo) => (
-          <TodoItem key={todo.id} {...todo} />
+          <TodoItem key={todo.id} {...todo} toggleTodo={toggleTodo} />
         ))}
       </ul>
     </>
--- frontend/nextjs-todo/src/components/TodoItem.tsx
+++ frontend/nextjs-todo/src/components/TodoItem.tsx
@@ -1,15 +1,22 @@
-import Link from "next/link";
+"use client";
 
 interface TodoItemProps {
   id: string;
   title: string;
   complete: boolean;
+  toggleTodo: (id: string, complete: boolean) => void;
 }
 
-export function TodoItem({ id, title, complete }: TodoItemProps) {
+export function TodoItem({ id, title, complete, toggleTodo }: TodoItemProps) {
   return (
     <li key={id} className="flex gap-1 items-center cursor-pointer">
-      <input id={id} type="checkbox" checked={complete} className="w-4 h-4 peer" />
+      <input
+        id={id}
+        type="checkbox"
+        className="w-4 h-4 peer"
+        defaultChecked={complete}
+        onChange={(e) => toggleTodo(id, e.target.checked)}
+      />
       <label htmlFor={id} className="peer-checked:line-through peer-checked:text-gray-500">
         {title}
       </label>
